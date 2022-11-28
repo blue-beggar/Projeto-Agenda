59351dc0f383e343297abc0faf597f6c99207e1a
blue-beggar <93293182+blue-beggar@users.noreply.github.com>
2022-11-28 20:33:46 -0300

    Add files via upload

---
+++ Projeto Agenda/src/projeto/agenda/ProjetoAgenda.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projeto.agenda;
+
+/**
+ *
+ * @author cg3017885
+ */
+public class ProjetoAgenda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Cadastro cadastro = new Cadastro();
+        
+        cadastro.setVisible(true);
+    }
+    
+}
