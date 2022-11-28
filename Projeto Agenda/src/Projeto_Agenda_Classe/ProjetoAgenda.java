3f420ffe91687b4881be0ed67bb5f232a532ad3f
EDUARDO <cg3018466@LAB203M21.IFSP>
2022-11-28 20:48:14 -0300

    Bifurcação

---
+++ Projeto Agenda/src/Projeto_Agenda_Classe/ProjetoAgenda.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Projeto_Agenda_Classe;
+
+import Projeto_Agenda_Tela.Cadastro_Tela;
+
+/**
+ *
+ * @author cg3017885
+ */
+public class ProjetoAgenda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Cadastro_Tela cadastro = new Cadastro_Tela();
+        
+        cadastro.setVisible(true);
+    }
+    
+}
